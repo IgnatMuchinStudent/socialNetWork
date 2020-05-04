69b4dfdaa80e5ba655f6dcd3eed1ec37a06215ef
Ignat <decl011@tut.by>
2020-05-04 11:19:42 +0300

    Updates

---
+++ precache-manifest.4af2474eaa5ac4d18755199d0a64fc39.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d66dbd7c2d576a17ffda09550ac568",
+    "url": "/socialNetWork/index.html"
+  },
+  {
+    "revision": "ba021dad05ce29b6308c",
+    "url": "/socialNetWork/static/css/3.118ddadc.chunk.css"
+  },
+  {
+    "revision": "b9db5fe063b218ed0095",
+    "url": "/socialNetWork/static/css/4.547ab8ce.chunk.css"
+  },
+  {
+    "revision": "9d9329f716c339769edb",
+    "url": "/socialNetWork/static/css/main.ce279807.chunk.css"
+  },
+  {
+    "revision": "cd8695ee3795ec00d7fd",
+    "url": "/socialNetWork/static/js/2.35812b93.chunk.js"
+  },
+  {
+    "revision": "ba021dad05ce29b6308c",
+    "url": "/socialNetWork/static/js/3.d92ce98d.chunk.js"
+  },
+  {
+    "revision": "b9db5fe063b218ed0095",
+    "url": "/socialNetWork/static/js/4.210926cd.chunk.js"
+  },
+  {
+    "revision": "9d9329f716c339769edb",
+    "url": "/socialNetWork/static/js/main.d3c164c4.chunk.js"
+  },
+  {
+    "revision": "74550e01a2ddf91cd5ea",
+    "url": "/socialNetWork/static/js/runtime~main.920bd57d.js"
+  },
+  {
+    "revision": "d48a8973a98231204ed9be58c6095785",
+    "url": "/socialNetWork/static/media/user.d48a8973.png"
+  }
+]);
